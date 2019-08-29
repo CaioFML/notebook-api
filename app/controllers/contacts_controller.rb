e9a939c41c525c237a38807498090cc3eb351c66
Caio <caio.felipemotta@hotmail.com>
2019-08-29 09:23:13 -0300

    22. I18n para data (as_json)

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,7 +11,7 @@ class ContactsController < ApplicationController
   # GET /contacts/1
   def show
     # root: true, status: :no_content (manda outro status.code), only: [:name, :email] e expect: [:email]
-    render json: @contact.to_br
+    render json: @contact, include: :kind
   end
 
   # POST /contacts
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,13 +2,19 @@ class Contact < ApplicationRecord
   has_many :phones
   belongs_to :kind # optional: true
 
-  def to_br
-    {
-      name: self.name,
-      email: self.email,
-      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank? )
-    }
+  def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank? )
+    h
   end
+
+  # def to_br
+  #   {
+  #     name: self.name,
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank? )
+  #   }
+  # end
 
   # def author
   #   "Caio"
