9acf5c10015cd4b5f4fc5f29f9247bff3e22c577
Caio <caio.felipemotta@hotmail.com>
2019-08-29 15:10:48 -0300

    33. Informações extras no JSON

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,19 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate #, :author
 
   belongs_to :kind
   has_many :phones
   has_one :address
+
+  # meta do
+  #   {
+  #     author: "Caio"
+  #   }
+  # end
+
+  # def author
+  #   'Caio'
+  # end
 
   def attributes(*args)
     h = super(*args)
