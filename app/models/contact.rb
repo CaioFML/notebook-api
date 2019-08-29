5c9ef1ed9cbf2f6a916e159d01552aba4c6e1e8d
Caio <caio.felipemotta@hotmail.com>
2019-08-29 10:56:08 -0300

    28. CORS

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,7 +5,7 @@ class Contact < ApplicationRecord
   belongs_to :kind # optional: true
 
   accepts_nested_attributes_for :phones, allow_destroy: true
-  accepts_nested_attributes_for :address, :update_only: true
+  accepts_nested_attributes_for :address, update_only: true
 
   def as_json(options={})
     h = super(options)
