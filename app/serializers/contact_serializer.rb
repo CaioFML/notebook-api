080695aa01726e9a4c836b2f14f965ec8e050a61
Caio <caio.felipemotta@hotmail.com>
2019-08-29 12:44:18 -0300

    31. Associações

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,10 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+
   def attributes(*args)
     h = super(*args)
     # h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank? ) # pt-BR
