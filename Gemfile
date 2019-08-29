9d4705bc39d6c30779287ef0c8c15bd5d9297796
Caio <caio.felipemotta@hotmail.com>
2019-08-29 15:36:11 -0300

    36. Correções, Ajustes e Foreman Gem

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'rails-i18n'
 
 gem 'active_model_serializers'
 
+# gem 'foreman'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,13 +2,13 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate #, :author
 
   belongs_to :kind do
-    link(:kind) { kind_url(object.kind.id) }
+    link(:related) { kind_url(object.kind.id) }
   end
 
   has_many :phones
   has_one :address
 
-  link(:self) { contact_url(object.id) }
+  # link(:self) { contact_url(object.id) }
 
   # meta do
   #   {
