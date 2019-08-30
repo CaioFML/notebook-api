6b36ff01cc141c9f6c018ae44658c41856a7033c
Caio <caio.felipemotta@hotmail.com>
2019-08-30 09:50:37 -0300

    46. CRUD AMS com has_one (Address)

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,43 @@
 class AddressesController < ApplicationController
-  before_action :set_address
+  before_action :set_contact
 
+  # GET /contacts/1/address
   def show
-    render json: @address
+    render json: @contact.address
+  end
+
+  # PATCH /contacts/1/address
+  def update
+    if @contact.address.update(address_params)
+      render json: @contact.address
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  # POST /contacts/1/address
+  def create
+    @contact.address = Address.new(address_params)
+
+    if @contact.save
+      render json: @contact.address, status: :created, location: contact_address_url(@contact) # Location envia via header a url de contact
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE/PUT /contacts/1
+  def destroy
+    @contact.address.destroy
   end
 
   private
 
-  def set_address
-    @address = Contact.find(params[:contact_id]).address
+  def set_contact
+    @contact = Contact.find(params[:contact_id])
+  end
+
+  def address_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   resources :kinds
   resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: 'relationships/kind'
+    resource :kind, only: %i[show]
+    resource :kind, only: %i[show], path: 'relationships/kind'
 
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationships/phones'
+    resource :phones, only: %i[show]
+    resource :phones, only: %i[show], path: 'relationships/phones'
 
-    resource :address, only: [:show]
-    resource :address, only: [:show], path: 'relationships/address'
+    resource :address, only: %i[show update create destroy]
+    resource :address, only: %i[show update create destroy], path: 'relationships/address'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
