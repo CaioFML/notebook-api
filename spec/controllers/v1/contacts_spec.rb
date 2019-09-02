690356e82aa400908b4ad4f5ecd2fb4ff57221fc
Caio <caio.felipemotta@hotmail.com>
2019-09-02 15:35:46 -0300

    78. Rspec (HTTP Status Code e .rspec)

--- spec/controllers/v1/contacts_spec.rb
+++ spec/controllers/v1/contacts_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe V1::ContactsController, type: :controller do
-  it 'request index and return status 200 OK - matcher have_http_status' do
+  it 'request index and return status 200, OK' do
     request.accept = 'application/vnd.api+json'
     get :index
-    expect(response).to have_http_status 200
+    expect(response).to have_http_status :ok
   end
 
   it 'request index and return status 406, not acceptable' do
     get :index
-    expect(response.status).to eq 406
+    expect(response).to have_http_status :not_acceptable
   end
 end
