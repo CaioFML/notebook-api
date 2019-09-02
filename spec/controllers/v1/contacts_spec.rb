43e012b29795dbc150b9f81ec1c14952f8e7a3be
Caio <caio.felipemotta@hotmail.com>
2019-09-02 16:20:40 -0300

    81. Melhorando a classe Hash

--- spec/controllers/v1/contacts_spec.rb
+++ spec/controllers/v1/contacts_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+class Hash
+  def json(parts)
+    ary = parts.split(">")
+    ary.reduce(self) do |memo, key|
+      memo.fetch(key.to_s.strip) if memo
+    end
+  end
+end
+
 RSpec.describe V1::ContactsController, type: :controller do
   it 'request index and return status 200, OK' do
     request.accept = 'application/vnd.api+json'
@@ -19,6 +28,8 @@ RSpec.describe V1::ContactsController, type: :controller do
     response_body = JSON.parse(response.body)
 
     expect(response_body.fetch('data').fetch('id')).to eq contact.id.to_s
+    expect(response_body.json('data > id')).to eq contact.id.to_s
     expect(response_body.fetch('data').fetch('type')).to eq 'contacts'
+    expect(response_body.json('data > type')).to eq 'contacts'
   end
 end
