6ffcc07b333e3c4edaffa4970990a68b5c246ded
Caio <caio.felipemotta@hotmail.com>
2019-09-02 12:57:40 -0300

    66. Desmistificando o Accept x Content-Type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :ensure_json_request
 
   def ensure_json_request
-    return if request.headers['Accept'] =~ /vnd\.api\+json/
+    unless request.headers['Accept'] =~ /vnd\.api\+json/
+      render nothing: true, status: 406
+    else
+      unless request.get?
+        return if request.headers["Content-Type"] =~ /vnd\.api\+json/
 
-    render nothing: true, status: 406
+        render nothing: true, status: 415
+      end
+    end
   end
 end
